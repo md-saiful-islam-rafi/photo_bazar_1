1a2f55a3b86dcbacaf3b456c34ba3c27bf342fdf
Md. Saiful Islam <md.saifulislam@Mds-Mac.local>
2020-01-27 14:09:00 +0600

    initial commit

--- Photo_Bazar_1/ViewController.swift
+++ Photo_Bazar_1/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var user_name: UITextField!
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBOutlet weak var confirm_password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func submit(_ sender: UIButton) {
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+            if error != nil {
+                print(error!)
+            }
+            else {
+                print("Successfully added")
+            }
+        }
+        
+        
+    }
 }
 
---
+++ Photo_Bazar_1_Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo_Bazar_1_Admin
+//
+//  Created by Md. Saiful Islam on 27/1/20.
+//  Copyright © 2020 Md. Saiful Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
